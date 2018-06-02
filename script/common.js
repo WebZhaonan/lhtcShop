0f38fada5869a65babe5b82d82c14c16777efe28
827745311@qq.com <827745311@qq.com@0e1a2743-8d03-4e5a-a60c-7e26af42b145>
2018-06-02 01:51:55 +0000

    更新
    git-svn-id: svn://svn4.apicloud.com/A6079750662654/%E4%B9%90%E8%88%AA%E5%90%8C%E5%9F%8E@15 0e1a2743-8d03-4e5a-a60c-7e26af42b145

--- script/common.js
+++ script/common.js
@@ -1,45 +1,14 @@
-function fnReady() {
-    fnReadyHeader();
-    fnReadyNav();
-    fnReadyFooter();
-};
 
-
-var header, headerHeight = 0;
-
-function fnReadyHeader() {
-    header = $api.byId('header');
-    if (header) {
-        $api.fixIos7Bar(header);
-        headerHeight = $api.offset(header).h;
-    }
-};
-
-var nav, navHeight = 0;
-
-function fnReadyNav() {
-    nav = $api.byId('nav');
-    if (nav) {
-        navHeight = $api.offset(nav).h;
-    }
-};
-
-var footer, footerHeight = 0;
-
-function fnReadyFooter() {
-    footer = $api.byId('footer');
-    if (footer) {
-        footerHeight = $api.offset(footer).h;
-    }
-};
 var hname = 0;
 //初始化
 function init() {
-  // 创建文件管理器
-
-  var data_=  api.readFile({
-        sync:true,
-        path: 'widget://res/common.json'
-    })
-    hname = JSON.parse(data_)
+  api.readFile({
+    path: 'widget://res/common.json'
+}, function(ret, err) {
+    if (ret.status) {
+        hname=JSON.parse(ret.data)
+    } else {
+        console.log(err.msg);
+    }
+});
 }
