39bf7ec027c0da984c6ac9bb4dbdede5261b5937
827745311@qq.com <827745311@qq.com@0e1a2743-8d03-4e5a-a60c-7e26af42b145>
2018-07-09 03:26:34 +0000

    提交
    git-svn-id: svn://svn4.apicloud.com/A6079750662654/%E4%B9%90%E8%88%AA%E5%90%8C%E5%9F%8E@45 0e1a2743-8d03-4e5a-a60c-7e26af42b145

---
+++ script/Map.js
@@ -0,0 +1,79 @@
+var map,
+    lat,
+    lon,
+    address;
+    // 打开地图
+    function fnBmap() {
+      map = api.require('bMap');
+      var Button = ['使用当前位置', '打开地图'];
+      api.actionSheet({
+      title: '请选择',
+      cancelTitle: '取消',
+      buttons: Button
+      }, function(ret, err) {
+      var index = ret.buttonIndex;
+      if(index ==1){
+      if(api.systemType == 'ios'){
+        api.getLocation(function(ret, err) {
+        if (ret && ret.status) {
+          map.initMapSDK(function(ret,err) {
+        if (ret.status) {
+            alert('地图初始化成功，可以从百度地图服务器检索信息了！');
+        }else {
+          {
+            alert(JSON.stringify(err))
+          }
+        }
+    });
+    }
+      });
+      }else{
+        map.getLocation({
+            accuracy: '100m',
+            autoStop: true,
+            filter: 1
+        }, function(ret, err){
+            api.hideProgress();
+            if(ret.status){
+                lat = ret.lat;
+                lon = ret.lon;
+                //设置地图中心
+                map.setCenter({
+                    coords: {
+                        lon: lon,
+                        lat: lat
+                    },
+                    animation:true
+                });
+                getNameFromCoords();
+            }
+
+        });
+      }
+    }else if (index==2) {
+      api.openWin({
+          name: 'bmap_win',
+          url: '../Map/bmap_win.html',
+          pageParam: {
+              name: 'test'
+          }
+      });
+    }
+    });
+    }
+    function getNameFromCoords(){
+        map.getNameFromCoords({
+            lon: lon,
+            lat: lat
+        },function(ret,err){
+            if(ret.status){
+                address = ret.address;
+                $api.val($api.byId("info"),address);
+            }else {
+                console.log(JSON.stringify(err));
+            }
+        });
+    }
+    function setInfo(address){
+        $api.val($api.byId("info"),address);
+    }
